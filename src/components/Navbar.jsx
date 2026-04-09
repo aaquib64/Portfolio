62853cbedcb14094faf3019fab0715afc7286e56
aaquib64 <aaquibshaikh64@gmail.com>
2026-04-09 17:18:12 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import {
   DisclosurePanel,
 } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -30,6 +30,7 @@ export default function Navbar() {
     >
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
+
           {/* Mobile Menu Button */}
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             <DisclosureButton className="inline-flex items-center justify-center rounded-md p-2 text-gray-300 hover:bg-gray-800 hover:text-white transition">
@@ -40,26 +41,26 @@ export default function Navbar() {
 
           {/* Logo */}
           <div className="flex items-center absolute left-1/2 -translate-x-1/2 sm:static sm:translate-x-0">
-            <a
-              href="/"
+            <Link
+              to="/"
               className="relative text-xl font-semibold text-white group"
             >
               Aaquib <span className="text-indigo-500">Shaikh</span>
               <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-indigo-500 group-hover:w-full transition-all duration-300"></span>
-            </a>
+            </Link>
           </div>
 
           {/* Desktop Navigation */}
           <div className="hidden sm:flex ml-auto items-center space-x-6">
             {navigation.map((item) => (
-              <a
+              <Link
                 key={item.name}
-                href={item.href}
+                to={item.href}
                 className={classNames(
                   "relative px-3 py-2 text-sm font-medium transition duration-300 group",
                   isActive(item.href)
                     ? "text-white"
-                    : "text-gray-300 hover:text-white",
+                    : "text-gray-300 hover:text-white"
                 )}
               >
                 {item.name}
@@ -68,10 +69,12 @@ export default function Navbar() {
                 <span
                   className={classNames(
                     "absolute left-0 -bottom-1 h-[2px] bg-indigo-500 transition-all duration-300",
-                    isActive(item.href) ? "w-full" : "w-0 group-hover:w-full",
+                    isActive(item.href)
+                      ? "w-full"
+                      : "w-0 group-hover:w-full"
                   )}
                 ></span>
-              </a>
+              </Link>
             ))}
           </div>
         </div>
@@ -83,13 +86,13 @@ export default function Navbar() {
           {navigation.map((item) => (
             <DisclosureButton
               key={item.name}
-              as="a"
-              href={item.href}
+              as={Link}
+              to={item.href}
               className={classNames(
                 "block rounded-md px-3 py-2 text-base font-medium transition",
                 isActive(item.href)
                   ? "bg-indigo-600 text-white"
-                  : "text-gray-300 hover:bg-gray-800 hover:text-white",
+                  : "text-gray-300 hover:bg-gray-800 hover:text-white"
               )}
             >
               {item.name}
